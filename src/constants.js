85d57e311aa2d135981ab52826612224128276a1
Davi Rodrigues <davirds07@gmail.com>
2022-02-17 18:06:14 -0300

    wagmiii

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const FACTORY_CONTRACT = "terra14rte4zp8mfljgg3rzmrejantdfa43j5tcuzzwk";
-export const PAIR_CONTRACT = "terra1e3fvmx098xyafu6ve77adhss6pv3lv5pzldfr3";
+export const PAIR_CONTRACT = "terra1fp8dedtq8lahflwnxafvmcupataqxvr7w0wm3l";
